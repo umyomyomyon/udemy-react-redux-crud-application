dcdd2dc59ab86df16beed7c3598d5de90c8ab035
umyomyomyon <MizukiOhshima@MacBook-Pro.local>
2019-12-17 22:31:22 +0900

    learn props and how to use default props

--- src/App.js
+++ src/App.js
@@ -15,18 +15,30 @@ class App extends Component {
 */
 
 const App = () => {
-  return (
-    <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-    </div>
-  );
+  const profiles = [
+    {name: 'Taro', age: 10},
+    {name: 'Haneko', age: 5},
+    {name: 'NoName'}
+  ]
+    return (
+      <div>
+        {
+          profiles.map((profile, index) => {
+            return (
+              <User name={profile.name} age={profile.age} key={index}/>
+            );
+          })
+        }
+      </div>
+    );
 }
-const Cat = () => {
-  return <div>cat</div>
+const User = (props) => {
+return <div>I`m {props.name}. and {props.age} years old.</div>
+}
+
+//指定のpropsが与えられない場合に、コンポーネントにデフォルトで受け渡されるpropsの値を決められる
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
