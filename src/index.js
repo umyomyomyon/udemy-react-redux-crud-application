b8fc6d7748a72066f75d557ad732b190f57835bc
umyomyomyon <MizukiOhshima@MacBook-Pro.local>
2019-12-19 00:36:24 +0900

    learn store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)//アプリケーション内部で唯一のstore
+//アプリケーション内部の全てのstateはこのstoreに集約されている
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
 import {combineReducers} from 'redux'
 //全てのReducerを結合するための機能
 import count from './count'
+
+export default combineReducers({count})
+//複数のReducerを引数に持ちたい場合
+//combineReducers({foo, bar, baz, ...})
