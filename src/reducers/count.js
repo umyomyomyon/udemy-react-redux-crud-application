a10d1ab603e2d6ed5c4d9d925c5ddd55e3907b62
umyomyomyon <MizukiOhshima@MacBook-Pro.local>
2019-12-18 22:48:09 +0900

    learn Reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,24 @@
+import {INCREMENT, DECREMENT} from '../actions'
+
+//状態の初期値を定義する(オブジェクトとして)
+//初期状態のオブジェクトを入れる変数名はinitialState
+const initialState = {value: 0}
+
+//countReducerを定義
+//reducers/index.jsに渡せるようにexportする
+//関数として定義する
+//引数は２つ
+//第一引数であるstateはデフォルト時には状態を持っていないので
+//デフォルト引数としてinitialStateを渡してあげる
+export default (state = initialState, action) => {
+    //受け取ったactionのtypeに応じて状態を変更して, その結果をreturnで返していく
+    switch (action.type) {
+        case INCREMENT:
+            return {value: state.value + 1}
+        case DECREMENT:
+            return {value: state.value - 1}
+        default:
+            return state 
+    }
+
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+import {combineReducers} from 'redux'
+//全てのReducerを結合するための機能
+import count from './count'
