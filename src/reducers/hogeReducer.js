c01b3da5be057ebeb7fc56d32eb78b731bc64a18
umyomyomyon <MizukiOhshima@MacBook-Pro.local>
2019-12-19 23:25:04 +0900

    fix typo

--- src/reducers/hogeReducer.js
+++ src/reducers/hogeReducer.js
@@ -2,11 +2,11 @@ const initialState = {value: 0}
 
 export default (state = initialState, action) => {
     console.log(`----------reducers/hogeReducer.js----------`)
-    console.log(`testState is ${JSON.stringify(state)}`)
+    console.log(`hogeState is ${JSON.stringify(state)}`)
     console.log(`actionType is ${JSON.stringify(action.type)}`)
     switch(action.type) {
         default:
-            console.log(`test switch is default.`)
+            console.log(`test switch case is default.`)
             return state
     }
 }
